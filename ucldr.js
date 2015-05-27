d6ef19d4053628c534d6ae8ba9e6b7ab271a4d5c
Paturages <a@faucheisti.eu>
2015-05-27 02:10:28 +0200

    Click behavior

--- ucldr.js
+++ ucldr.js
@@ -1,122 +1,113 @@
 'use strict';
 
-angular.module('ngUcldr', []).filter('ucldr_f_days', function() {
-	return function(input, nb_days, day_start) {
+angular.module('ngUcldr', []).filter('f1', function() {
+	return function(a, n, s) {
 		var i, j, d = 1;
-		for (i = 0 ; i < day_start ; i++) {
-			input[0][i] = "";
-		}
-		for (i = day_start ; i < 7 ; i++) {
-			input[0][i] = d++;
-		}
-		for (i = 1 ; i < 6 ; i++) {
-			for (j = 0 ; j < 7 ; j++) {
-				if (d <= nb_days) {
-					input[i][j] = d++;
-				} else {
-					input[i][j] = "";
-				}
-			}
-		}
-		return input;
+		for (i = 0 ; i < s ; i++)
+			a[0][i] = "";
+		for (i = s ; i < 7 ; i++)
+			a[0][i] = d++;
+		for (i = 1 ; i < 6 ; i++)
+			for (j = 0 ; j < 7 ; j++)
+				if (d <= n)
+					a[i][j] = d++;
+				else
+					a[i][j] = "";
+		return a;
 	};
-}).filter('ucldr_f_month', function() {
-	return function(input, c_year, n_month, n_year) {
-		var tmp = input.slice();
-		if (c_year == n_year) {
-			for (var i = n_month+1 ; i < 12 ; i++) {
-				tmp.pop();
-			}
-		}
-		return tmp;
-	};
-}).directive('ngUcldr', function() {
+}).directive('ngUcldr', ['$window',
+function(w) {
 	return {
 		restrict: 'E',
 		scope: {
-			"field": "=ucldrModel"
+			"f": "=bindTo"
 		},
 		controller: function($scope) {
-			var now = new Date();
-			$scope.months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-			$scope.days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-			$scope.now = {
-				day: now.getDate(),
-				month: now.getMonth(),
-				year: now.getFullYear()
-			};
-			$scope.years = [];
-			for (var i = 0 ; i < 20 ; i++) {
-				$scope.years[i] = $scope.now.year-i;
-			}
-			$scope.c = {
-				day: $scope.now.day,
-				month: $scope.now.month,
-				year: $scope.now.year,
-				getNbDays: function() {
-					return (new Date(this.year, parseInt(this.month)+1, 0)).getDate();
-				},
-				getDayStart: function() {
-					var res = (new Date(this.year, this.month, 1)).getDay();
-					return (res == 6) ? 0 : res+1;
-				},
-				toString: function() {
-					return this.year+"-"+(parseInt(this.month)+1)+"-"+this.day;
-				},
-				decrMonth: function() {
-					if (this.month == 0) {
-						this.month = 11;
-						this.year--;
-					} else {
-						this.month--;
-					}
-				},
-				incrMonth: function() {
-					if ((this.year < $scope.now.year) || (this.month < $scope.now.month)) {
-						if (this.month == 11) {
-							this.month = 0;
-							this.year++;
-						} else {
-							this.month++;
-						}
-					}
+			var s = $scope;
+			var n = new Date();
+			s.s = false;
+			s.tg = function(d) {
+				if (d && (s.c.y < s.n.y || s.c.m < s.n.m || d <= s.n.d)) {
+					s.s = !s.s || !d;
+					s.c.d = d;
 				}
 			}
-			$scope.dateEval = function(day) {
-				if (day == "") {
-					return "";
-				} if ($scope.c.year > $scope.now.year) {
-				return "ucldr_future";
-				} else if ($scope.c.year < $scope.now.year) {
-					return "ucldr_past";
-				} else if ($scope.c.month > $scope.now.month) {
-					return "ucldr_future";
-				} else if ($scope.c.month < $scope.now.month) {
-					return "ucldr_past";
-				} else if (day == $scope.c.day) {
-					return "ucldr_selected";
-				} else if (day > $scope.now.day) {
-					return "ucldr_future";
-				} else if (day < $scope.now.day) {
-					return "ucldr_past";
+			s.lm = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+			s.ld = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+			s.n = {
+				d: n.getDate(),
+				m: n.getMonth(),
+				y: n.getFullYear()
+			};
+			s.ly = [];
+			for (var i = 0 ; i < 20 ; i++)
+				s.ly[i] = s.n.y-i;
+			s.gnd = function() {
+				return (new Date(s.c.y, +s.c.m+1, 0)).getDate();
+			};
+			s.gds = function() {
+				var res = (new Date(s.c.y, s.c.m, 1)).getDay();
+				return (res == 6) ? 0 : res+1;
+			};
+			s.ts = function() {
+				var m = (s.c.m < 9) ? "0"+(+s.c.m+1) : (+s.c.m+1), d = (s.c.d < 10) ? "0"+s.c.d : s.c.d;
+				return s.c.y+"-"+m+"-"+d;
+			};
+			s.dm = function() {
+				if (s.c.m) {
+					s.c.m--;
 				} else {
-					return "ucldr_today";
+					s.c.m = 11;
+					s.c.y--;
 				}
 			};
-			$scope.tab = [[],[],[],[],[],[]];
+			s.im = function() {
+				if ((s.c.y < s.n.y) || (s.c.m < s.n.m)) {
+					if (s.c.m == 11) {
+						s.c.m = 0;
+						s.c.y++;
+					} else {
+						s.c.m++;
+					}
+				}
+			}
+			s.c = {
+				d: s.n.d,
+				m: s.n.m,
+				y: s.n.y
+			}
+			s.de = function(d) {
+				if (d == "")
+					return "";
+				else if (d == s.c.d)
+					return "slct";
+				else if (s.c.y > s.n.y)
+					return "ftr";
+				else if (s.c.y < s.n.y)
+					return "pst";
+				else if (s.c.m > s.n.m)
+					return "ftr";
+				else if (s.c.m < s.n.m)
+					return "pst";
+				else if (d > s.n.d)
+					return "ftr";
+				else if (d < s.n.d)
+					return "pst";
+				else
+					return "tdy";
+			};
+			s.t = [[],[],[],[],[],[]];
 		},
-		template:'<div class="ucldr-text">{{c.toString()}}</div><table class="ucldr"><thead><tr><th><button ng-click="c.decrMonth()">&lt;</button></th><th colspan=5><select class="ucldr_month" ng-model="c.month"><option ng-repeat="m in months | ucldr_f_month: c.year: now.month: now.year" value="{{$index}}" ng-selected="{{now.month == $index}}">{{m}}</option></select><select class="ucldr_year" ng-model="c.year" ng-options="y for y in years"></select></th><th><button ng-click="c.incrMonth()">&gt;</button></th></tr></thead><tbody><tr><th ng-repeat="day in days">{{day}}</th></tr><tr ng-repeat="week in tab | ucldr_f_days: c.getNbDays(): c.getDayStart()"><td ng-repeat="day in week track by $index" class="{{dateEval(day)}}" ng-click="c.day = day">{{day}}</td></tr></tbody></table>',
-		link: function(scope, elt, attrs) {
-			elt[0].childNodes[1].hidden = true;
-			elt[0].childNodes[0].onclick = function() {
-				elt[0].childNodes[1].hidden = !elt[0].childNodes[1].hidden;
-			};
-			scope.field = {
-				day: scope.c.day,
-				month: scope.c.month,
-				year: scope.c.year,
-				toString: scope.c.toString
-			};
+		template:'<div class="ucldr-text" ng-click="s=!s">{{ts()}}</div><table class="ucldr" ng-if="s"><thead><tr><th><button ng-click="dm()">&lt;</button></th><th colspan=5><select class="ucldr_month" ng-model="c.m"><option ng-repeat="m in lm" value="{{$index}}" ng-selected="c.m==$index" ng-disabled="c.y==n.y&&$index>n.m">{{m}}</option></select><select class="ucldr_year" ng-model="c.y" ng-options="y for y in ly"></select></th><th><button ng-click="im()">&gt;</button></th></tr></thead><tbody><tr><th ng-repeat="d in ld">{{d}}</th></tr><tr ng-repeat="w in t | f1: gnd(): gds()"><td ng-repeat="d in w track by $index" class="{{de(d)}}" ng-click="tg(d)">{{d}}</td></tr></tbody></table>',
+		link: function(s, e) {
+			s.f = s.c;
+			e.bind('click', function(x) {
+				x.stopPropagation();
+			});
+			angular.element(w).on('click', function() {
+				s.s = false;
+				s.$apply();
+			});
 		}
 	};
-});
+}]);
